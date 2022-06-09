6d9641d999133713d15d51d80686dacff3427bc5
Sebastian <sebasfedele@gmail.com>
2022-06-09 18:23:27 -0300

    resultado de promedios

--- clase8/Program.cs
+++ clase8/Program.cs
@@ -7,18 +7,20 @@ int columnas = int.Parse( Console.ReadLine());
 Console.WriteLine();
 
 int[,] numeros = new int[filas, columnas];
-//double[] promedio = new double[];
+double suma = 0;
+double[] promedios = new double[columnas];
 
 for (int i = 0; i < filas; i++)
 {
-    Console.Write($"valores de fila: {i +1}");
-        Console.WriteLine();
+    Console.WriteLine();
+    Console.Write($"Para valor/es de fila: {i +1}");
+    Console.WriteLine();
     for(int j = 0; j < columnas; j++)
     {
-        Console.Write($"Ingrese valor de columna {j + 1}: ");
+        Console.Write($"Ingrese valor/es de columna {j + 1}: ");
         int valor = int.Parse( Console.ReadLine());
-        numeros[i,j] = valor;    
-    }
+        numeros[i,j] = valor;
+     }
     Console.WriteLine();
 }
 Console.WriteLine("Los valores son");
@@ -26,8 +28,26 @@ for (int i = 0; i < filas; i++)
 {
    for (int j = 0; j < columnas; j++)
     {
-        Console.Write($"{numeros[i,j]:000}\t");
-     
-    }
+        Console.Write($"{numeros[i,j]:0000}\t");
+     }
     Console.WriteLine();
+}
+Console.WriteLine();
+
+
+
+
+for (int i = 0; i < columnas; i++)
+{
+    for (int j = 0; j < filas; j++)
+    {
+        suma =+ numeros[j, i];
+    }
+    promedios[i] = suma / filas;
+}
+Console.WriteLine();
+Console.WriteLine("Los promedios son:");
+for(int i = 0; i < columnas; i++)
+{
+    Console.WriteLine($"{promedios[i]}");
 }
