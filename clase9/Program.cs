0a16a404b45da6bcc50daf6e5c4ae824671a0896
Sebastian <sebasfedele@gmail.com>
2022-06-12 21:10:53 -0300

    pedido de datos de array y muestra

---
+++ clase9/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ingrese la longitud de vector: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int [] vector = new int [n];
+
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Ingrese valor:");
+    int valor = int.Parse(Console.ReadLine());
+    vector[i] = valor;
+}
+foreach(int i in vector)
+{
+    Console.Write( $" {i} \t");
+}
