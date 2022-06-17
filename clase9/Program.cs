e9cf6af8c38be7e251e0411e0ea893af54682bc2
Sebastian <sebasfedele@gmail.com>
2022-06-16 23:19:00 -0300

    clase 9 ejercicio sin dar vuelta array

--- clase9/Program.cs
+++ clase9/Program.cs
@@ -3,15 +3,34 @@ int n = int.Parse(Console.ReadLine());
 
 
 int [] vector = new int [n];
-
+int j = 0;
 
 for (int i = 0; i < n; i++)
 {
     Console.WriteLine("Ingrese valor:");
     int valor = int.Parse(Console.ReadLine());
     vector[i] = valor;
+    j++;
 }
-foreach(int i in vector)
+Console.WriteLine("Los valores del vector son:");
+foreach (int i in vector)
 {
-    Console.Write( $" {i} \t");
+        Console.Write( $" {i} \t");
 }
+//Reverse().ToArray(); herramienta de C#
+Console.WriteLine();
+
+for (int i = j; i > 0; i--)
+{
+    int aux = vector[vector.Length - i];
+    vector[i-1] = aux;
+   }
+
+Console.WriteLine("Los valores del vector son:");
+foreach (int i in vector)
+{
+
+    Console.Write($" {i} \t");
+
+}
+Console.ReadKey();
